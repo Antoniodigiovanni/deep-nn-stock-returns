ef44307bd6d9ca49f21676fbb96ad97f030b6b46
Antoniodigiovanni <antoniodg98@gmail.com>
2022-05-31 15:44:44 +0200

    Added config

--- wrdsJobScript/job.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#$ -cwd
-#$ -m abe
-#$ -M a.di.giovanni@tum.de
-echo "Starting Job at `date`"
-R CMD BATCH /home/tum/antoniodg/thesis/src/data/OpenAssetPricing_Code/master.R
-echo "Ending Job at `date`"
