31d582dccc4ab36cb76f3d273babbfd4eb2b4441
Antoniodigiovanni <antoniodg98@gmail.com>
2022-09-24 19:45:59 +0000

    Create new Top20 plot and fix previous one

--- src/trainer/feature_importance.py
+++ src/trainer/feature_importance.py
@@ -7,14 +7,14 @@ from torch.utils.data import DataLoader
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
+import pandas as pd
 
 def IntegratedGradients_importance(model, path):
     ig = IntegratedGradients(model)
     dataset = BaseDataset()
     df = dataset.df
 
-    feature_names = list(df.drop(['permno','ret','yyyymm','melag','me','prc','me_nyse20'], axis=1).columns)
-    print(feature_names)
+    feature_names = list(df.drop(['permno','ret','yyyymm','melag','me','prc','me_nyse20'], axis=1, errors='ignore').columns)
     df = df.loc[df['yyyymm'] >= 199501]
     df = CrspDataset(df)
     test_loader = DataLoader(df, batch_size=100)
@@ -30,7 +30,11 @@ def IntegratedGradients_importance(model, path):
     visualize_importances(feature_names, np.mean(attr, axis=0), path)
 
 def visualize_importances(feature_names, importances, path, title="Average Feature Importances", plot=True, axis_title="Features"):
-    print(title)
+    
+    importance_df = pd.DataFrame({'feature': feature_names, 'importance': importances})
+    importance_df = importance_df.reindex(importance_df.importance.abs().sort_values(ascending=False).index)
+    importance_df_top = importance_df.head(20) 
+
     for i in range(len(feature_names)):
         print(feature_names[i], ": ", '%.3f'%(importances[i]*1000000000))
     x_pos = (np.arange(len(feature_names)))
@@ -42,12 +46,42 @@ def visualize_importances(feature_names, importances, path, title="Average Featu
         # plt.title(title)
         # plt.savefig("feature_importance.png")
 
-        plt.figure(figsize=(50,20))
-        plt.bar(x_pos, importances, align='center')
-        plt.xticks(x_pos, feature_names, wrap=True)
-        plt.xlabel(axis_title)
-        plt.title(title)
-        plt.xticks(rotation=45)
-        plt.tight_layout()
+        # plt.figure(figsize=(50,20))
+        # plt.bar(x_pos, importances, align='center')
+        # plt.xticks(x_pos, feature_names, wrap=True)
+        # plt.xlabel(axis_title)
+        # plt.title(title)
+        # plt.xticks(rotation=45)
+        # plt.tight_layout()
+        # plt.savefig(path)
+
+        path_top = path + '_feature_importance_top20.png'
+        path = path + '_feature_importance.png'
+
+        x_pos = (np.arange(len(importance_df['feature'])))
+
+        fig = plt.figure(figsize=(20,10))
+        ax = plt.axes()
+        ax.set_title('Feature Importance', fontsize=25)
+        ax.set_xticks(x_pos)
+        ax.set_xticklabels(importance_df['feature'], rotation=45, ha='right', fontsize=14)
+        ax.bar(x_pos, importance_df['importance'],align='center',  zorder=3)
+        ax.xaxis.grid(True, linestyle='--',  zorder=0)
+        plt.axhline(y=0, color='grey', linestyle='-')
+        fig.tight_layout()
         plt.savefig(path)
+
+        # Top 20 plot
+        x_pos = (np.arange(len(importance_df_top['feature'])))
+
+        fig = plt.figure(figsize=(20,10))
+        ax = plt.axes()
+        ax.set_title('Feature Importance - Top 20 (of '+str(len(feature_names))+')', fontsize=25)
+        ax.set_xticks(x_pos)
+        ax.set_xticklabels(importance_df_top['feature'], rotation=45, ha='right', fontsize=14)
+        ax.bar(x_pos, importance_df_top['importance'],align='center',  zorder=3)
+        ax.xaxis.grid(True, linestyle='--',  zorder=0)
+        plt.axhline(y=0, color='grey', linestyle='-')
+        fig.tight_layout()
+        plt.savefig(path_top)
 
